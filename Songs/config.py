568f232dc08616d9ec01cebc89fa37efe8c86fcc
AlexanderGroeger <46076580+AlexanderGroeger@users.noreply.github.com>
2020-12-16 13:39:23 -0500

    config.py is no longer being tracked

--- Songs/config.py
+++
@@ -1,5 +0,0 @@
-# YOU MUST USE DOUBLE BACKSLASHES IN PATHS!!!
-outputSongPath = "D:\\Users\\Alex\\GamesAndDev\\KappaCraft\\Songs\\songs"
-outputSongTreePath = "D:\\Users\\Alex\\GamesAndDev\\KappaCraft\\Songs\\songs\\trees"
-# Directory path for functions to call functions inside a song tree
-functionBranchPrefix = "mcfunctions:songs:trees"
