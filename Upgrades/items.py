d6eb5db023f6f057d175f4d33715c3ffb928f3c2
AlexanderGroeger <46076580+AlexanderGroeger@users.noreply.github.com>
2020-12-30 13:58:06 -0500

    added more items

--- Upgrades/items.py
+++ Upgrades/items.py
@@ -4,14 +4,50 @@ items = {
         "levels": [1,2,3,4,5,7,10,15,25,40,65,100,250],
         "cost":   [0,0,0,0,1,2, 2, 2,10,30,40, 50, 0],
     },
+    "diamond_helmet": {
+        "enchantment": "protection",
+        "levels": [1,2,3,4,5],
+        "cost":   [2,3,4,30,0],
+    },
+    "diamond_chestplate": {
+        "enchantment": "protection",
+        "levels": [1,2,3,4,5],
+        "cost":   [2,3,4,30,0],
+    },
+    "diamond_pants": {
+        "enchantment": "protection",
+        "levels": [1,2,3,4,5],
+        "cost":   [2,3,4,30,0],
+    },
+    "diamond_boots": {
+        "enchantment": "protection",
+        "levels": [1,2,3,4,5],
+        "cost":   [2,3,4,30,0],
+    },
+
     "netherite_sword": {
         "enchantment": "sharpness",
         "levels": [1,2,3,4,5,8,12,20,40,80,130,200,500],
         "cost":   [0,0,0,0,1,2, 2, 2,10,30,40, 50, 0],
     },
-    "diamond_chestplate": {
+    "netherite_helmet": {
         "enchantment": "protection",
         "levels": [1,2,3,4,5],
-        "cost":   [2,5,10,30,0],
+        "cost":   [2,3,4,30,0],
+    },
+    "netherite_chestplate": {
+        "enchantment": "protection",
+        "levels": [1,2,3,4,5],
+        "cost":   [2,3,4,30,0],
+    },
+    "netherite_pants": {
+        "enchantment": "protection",
+        "levels": [1,2,3,4,5],
+        "cost":   [2,3,4,30,0],
+    },
+    "netherite_boots": {
+        "enchantment": "protection",
+        "levels": [1,2,3,4,5],
+        "cost":   [2,3,4,30,0],
     },
 }
