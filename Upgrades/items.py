f82f2ccd59fec4a7e32747d3b367d542c1c4a4ba
AlexanderGroeger <46076580+AlexanderGroeger@users.noreply.github.com>
2020-12-30 18:48:25 -0500

    added more items

--- Upgrades/items.py
+++ Upgrades/items.py
@@ -25,8 +25,36 @@ items = {
         {"cost": 40, "protection": 6, "blast_protection": 1, "projectile_protection": 1, "fire_protection": 1},
         {"cost": 50, "protection": 7, "blast_protection": 2, "projectile_protection": 2, "fire_protection": 2},
         {
-            "protection": 10, "blast_protection": 3, "projectile_protection": 3, "fire_protection": 3,
-            "attributes": """,AttributeModifiers:[{AttributeName:"generic.max_health",Amount:2,Operation:0,UUID:[I;-1201130,31815,173936,-63630],Slot:head,Name:"generic.max_health"},{AttributeName:"generic.attack_damage",Amount:0.1,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:head,Name:"generic.attack_damage"},{AttributeName:"generic.movement_speed",Amount:0.1,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:head,Name:"generic.movement_speed"}]"""
+            "protection": 10, "blast_protection": 3, "projectile_protection": 3, "fire_protection": 3, "aqua_affinity": 1, "thorns": 5, "respiration": 10,
+            "attributes": """,AttributeModifiers:[{AttributeName:"generic.max_health",Amount:2,Operation:0,UUID:[I;-1201130,31815,173936,-63630],Slot:head,Name:"generic.max_health"},{AttributeName:"generic.attack_damage",Amount:0.1,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:head,Name:"generic.attack_damage"},{AttributeName:"generic.movement_speed",Amount:0.1,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:head,Name:"generic.movement_speed"}],Unbreakable: 1, display:{Lore:['[{"text":"Perfectly crafted","italic":false,"color":"dark_purple"}]','[{"text":"This item will not be lost on death","italic":false,"color":"dark_purple"}]']}"""
+        },
+    ),
+
+    "diamond_chestplate": (
+        {"cost": 2, "protection": 1},
+        {"cost": 3, "protection": 2},
+        {"cost": 4, "protection": 3},
+        {"cost": 10, "protection": 4},
+        {"cost": 25, "protection": 5},
+        {"cost": 40, "protection": 6, "blast_protection": 1, "projectile_protection": 1, "fire_protection": 1},
+        {"cost": 50, "protection": 7, "blast_protection": 2, "projectile_protection": 2, "fire_protection": 2},
+        {
+            "protection": 10, "blast_protection": 4, "projectile_protection": 4, "fire_protection": 4, "thorns": 5,
+            "attributes": """,AttributeModifiers:[{AttributeName:"generic.max_health",Amount:4,Operation:0,UUID:[I;-1201130,31815,173936,-63630],Slot:chest,Name:"generic.max_health"},{AttributeName:"generic.attack_damage",Amount:0.15,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:chest,Name:"generic.attack_damage"},{AttributeName:"generic.movement_speed",Amount:0.1,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:chest,Name:"generic.movement_speed"}],Unbreakable: 1, display:{Lore:['[{"text":"Perfectly crafted","italic":false,"color":"dark_purple"}]','[{"text":"This item will not be lost on death","italic":false,"color":"dark_purple"}]']}"""
+        },
+    ),
+
+    "diamond_leggings": (
+        {"cost": 2, "protection": 1},
+        {"cost": 3, "protection": 2},
+        {"cost": 4, "protection": 3},
+        {"cost": 10, "protection": 4},
+        {"cost": 25, "protection": 5},
+        {"cost": 40, "protection": 6, "blast_protection": 1, "projectile_protection": 1, "fire_protection": 1},
+        {"cost": 50, "protection": 7, "blast_protection": 2, "projectile_protection": 2, "fire_protection": 2},
+        {
+            "protection": 10, "blast_protection": 4, "projectile_protection": 4, "fire_protection": 4, "thorns": 5,
+            "attributes": """,AttributeModifiers:[{AttributeName:"generic.max_health",Amount:4,Operation:0,UUID:[I;-1201130,31815,173936,-63630],Slot:legs,Name:"generic.max_health"},{AttributeName:"generic.attack_damage",Amount:0.15,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:pants,Name:"generic.attack_damage"},{AttributeName:"generic.movement_speed",Amount:0.1,Operation:1,UUID:[I;-1201130,31815,173936,-63630],Slot:legs,Name:"generic.movement_speed"}],Unbreakable: 1, display:{Lore:['[{"text":"Perfectly crafted","italic":false,"color":"dark_purple"}]','[{"text":"This item will not be lost on death","italic":false,"color":"dark_purple"}]']}"""
         },
     ),
 
