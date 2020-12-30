17739658742283a68f721a4849cb9ea747d516a2
AlexanderGroeger <46076580+AlexanderGroeger@users.noreply.github.com>
2020-12-30 11:42:11 -0500

    fixed odd levels 's'

--- Upgrades/cmds.py
+++ Upgrades/cmds.py
@@ -9,7 +9,7 @@ def Format(s,**kwargs):
     return s
 
 
-main = """execute at @p[nbt={SelectedItem:{id:"minecraft:___item",tag:{Enchantments:[{id:"minecraft:___enchantment",lvl:___oldLvls}]}}},nbt={Inventory:[{Slot:-106b,id:"minecraft:___item",tag:{Enchantments:[{id:"minecraft:___enchantment",lvl:___oldLvls}]}}]},scores={upgrade=1..,xpLevel=___levelsNeeded..}] run scoreboard players set @p upgrading 1"""
+main = """execute at @p[nbt={SelectedItem:{id:"minecraft:___item",tag:{Enchantments:[{id:"minecraft:___enchantment",lvl:___oldLvl}]}}},nbt={Inventory:[{Slot:-106b,id:"minecraft:___item",tag:{Enchantments:[{id:"minecraft:___enchantment",lvl:___oldLvl}]}}]},scores={upgrade=1..,xpLevel=___levelsNeeded..}] run scoreboard players set @p upgrading 1"""
 upgrade = """replaceitem entity @p[scores={upgrading=1..}] weapon.mainhand minecraft:___item{Enchantments:[{id:"minecraft:___enchantment",lvl:___newLvl}]}"""
 clear = "replaceitem entity @p[scores={upgrading=1..}] weapon.offhand minecraft:air"
 removeLevels = "xp add @p[scores={upgrading=1..}] -___levelsNeeded levels"
