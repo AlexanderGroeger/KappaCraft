b0a44bed16fb58111c7deed1ef69635d01bb8b66
AlexanderGroeger <46076580+AlexanderGroeger@users.noreply.github.com>
2021-01-01 15:14:07 -0500

    added colored shulkers

--- Inventory/build_keep_inventory.py
+++ Inventory/build_keep_inventory.py
@@ -3,9 +3,10 @@ from slots import slots
 
 lines = []
 for slotNum, slotName in slots.items():
-    lines.append(Format(clear, slotNum = slotNum, slotName = slotName))
+    shulkerTargeting = ",".join([Format(shulker, color = color+"_") for color in colors]+[Format(shulker, color = "")])
+    lines.append(Format(Format(clear, shulkerTargeting = shulkerTargeting), slotNum = slotNum, slotName = slotName))
 for slotNum, slotName in slots.items():
-    lines.append(Format(vanishClear, slotNum = slotNum, slotName = slotName))    
+    lines.append(Format(vanishClear, slotNum = slotNum, slotName = slotName))
 
 with open("keep_inventory.mcfunction", 'w') as f:
     f.write("\n".join(lines))
--- Inventory/cmds.py
+++ Inventory/cmds.py
@@ -8,5 +8,7 @@ def Format(s,**kwargs):
         s = s.replace("___"+key+"___",str(value))
     return s
 
-clear = """execute at @a[nbt=!{Inventory:[{id:"minecraft:shulker_box",Slot:___slotNum___b}]},scores={died=1}] run replaceitem entity @a[nbt=!{Inventory:[{Slot:___slotNum___b,tag:{Keep:1b}}]},scores={died=1,keepinventory=0}] ___slotName___ air"""
+colors = ["white","orange","magenta","light_blue","yellow","lime","pink","gray","light_gray","cyan","purple","blue","brown","green","red","black"]
+shulker = """nbt=!{Inventory:[{id:"minecraft:___color___shulker_box",Slot:___slotNum___b}]},scores={died=1}"""
+clear = """execute at @a[] run replaceitem entity @a[nbt=!{Inventory:[{Slot:___slotNum___b,tag:{Keep:1b}}]},___shulkerTargeting___,scores={died=1,keepinventory=0}] ___slotName___ air"""
 vanishClear = """replaceitem entity @a[nbt={Inventory:[{Slot:___slotNum___b,tag:{Enchantments:[{id:"minecraft:vanishing_curse",lvl:1s}]}}]}] ___slotName___ air"""
