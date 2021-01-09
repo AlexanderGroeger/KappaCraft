244b91fbb83f4250a756cd739a2ad96480f78c0c
wiihacker0 <wiihacker0@gmail.com>
2021-01-09 15:20:20 -0500

    take a guess lol

--- KappaMart/alex_shop.py
+++ KappaMart/alex_shop.py
@@ -2,300 +2,292 @@ shopDeals = {
     # Refridgered Section
     "egg": {
         "Name": ["Egg","Eggs"],
-        "Buy": {"n": 12, "$": 2, "p": "29999895 192 29999870"},
+        "Buy": {"n": 12, "$": 2, "p": "37 113 42"},
     },
     "milk_bucket": {
         "Name": ["Moomoo Milk","Moomoo Milk"],
-        "Buy": {"n": 1, "$": 5, "p": "29999894 192 29999870"},
+        "Buy": {"n": 1, "$": 5, "p": "36 113 42"},
     },
     "ice": {
         "Name": ["Ice Cube","Ice Cubes"],
-        "Buy": {"n": 12, "$": 1, "p": "29999893 192 29999870"},
+        "Buy": {"n": 12, "$": 1, "p": "35 113 42"},
     },
     "blue_ice": {
         "Name": ["Blue Ice","Blue Ice"],
-        "Buy": {"n": 1, "$": 4, "p": "29999892 192 29999870"},
+        "Buy": {"n": 1, "$": 4, "p": "34 113 42"},
     },
     # Lights
     "torch": {
         "Name": ["Torch","Torches"],
-        "Buy": {"n": 16, "$": 1, "p": "29999893 193 29999852"},
+        "Buy": {"n": 16, "$": 1, "p": "34 114 24"},
     },
     "lantern": {
         "Name": ["Lantern","Lanterns"],
-        "Buy": {"n": 8, "$": 1, "p": "29999893 193 29999853"},
+        "Buy": {"n": 8, "$": 1, "p": "34 114 25"},
     },
     "campfire": {
         "Name": ["Campfire","Campfires"],
-        "Buy": {"n": 2, "$": 1, "p": "29999893 193 29999854"},
+        "Buy": {"n": 2, "$": 1, "p": "34 114 26"},
     },
     "sea_lantern": {
         "Name": ["Sea Lantern","Sea Lanterns"],
-        "Buy": {"n": 4, "$": 1, "p": "29999893 193 29999855"},
+        "Buy": {"n": 4, "$": 1, "p": "34 114 27"},
     },
     # Precious Minerals
     "iron_ingot": {
         "Name": ["Iron Ingot","Iron Ingots"],
-        "Buy": {"n": 1, "$": 1, "p": "29999878 193 29999856"},
+        "Buy": {"n": 1, "$": 1, "p": "20 114 28"},
     },
     "iron_block": {
         "Name": ["Iron Block","Iron Blocks"],
-        "Sell": {"n": 1, "$": 4, "p": "29999878 192 29999856"},
+        "Sell": {"n": 1, "$": 4, "p": "20 113 28"},
     },
     "gold_ingot": {
         "Name": ["Gold Ingot","Gold Ingots"],
-        "Buy": {"n": 1, "$": 4, "p": "29999878 193 29999857"},
-        "Sell": {"n": 1, "$": 2, "p": "29999878 192 29999857"},
+        "Buy": {"n": 1, "$": 4, "p": "20 114 29"},
+        "Sell": {"n": 1, "$": 2, "p": "20 113 29"},
     },
     "diamond": {
         "Name": ["Diamond","Diamonds"],
-        "Buy": {"n": 1, "$": 25, "p": "29999878 193 29999858"},
-        "Sell": {"n": 1, "$": 10, "p": "29999878 192 29999858"},
+        "Buy": {"n": 1, "$": 25, "p": "20 114 30"},
+        "Sell": {"n": 1, "$": 10, "p": "20 113 30"},
     },
     "lapis_lazuli": {
         "Name": ["Lapis","Lapis"],
-        "Buy": {"n": 5, "$": 2, "p": "29999878 193 29999859"},
-        "Sell": {"n": 12, "$": 1, "p": "29999878 192 29999859"},
+        "Buy": {"n": 5, "$": 2, "p": "20 114 31"},
+        "Sell": {"n": 12, "$": 1, "p": "20 113 31"},
     },
     "redstone": {
         "Name": ["Redstone","Redstone"],
-        "Buy": {"n": 5, "$": 2, "p": "29999878 193 29999860"},
-        "Sell": {"n": 12, "$": 1, "p": "29999878 192 29999860"},
+        "Buy": {"n": 5, "$": 2, "p": "20 114 32"},
+        "Sell": {"n": 12, "$": 1, "p": "20 113 32"},
     },
     "emerald": {
         "Name": ["Emerald","Emeralds"],
-        "Buy": {"n": 1, "$": 6, "p": "29999878 193 29999861"},
-        "Sell": {"n": 1, "$": 3, "p": "29999878 192 29999861"},
+        "Buy": {"n": 1, "$": 6, "p": "20 114 33"},
+        "Sell": {"n": 1, "$": 3, "p": "20 113 33"},
     },
     # Blocks
     "dirt": {
         "Name": ["Dirt","Dirt"],
-        "Buy": {"n": 32, "$": 1, "p": "29999880 193 29999861"},
-        "Sell": {"n": 64, "$": 1, "p": "29999880 192 29999861"},
+        "Buy": {"n": 32, "$": 1, "p": "22 114 33"},
+        "Sell": {"n": 64, "$": 1, "p": "22 113 33"},
     },
     "cobblestone": {
         "Name": ["Rocks","Rocks"],
-        "Buy": {"n": 32, "$": 1, "p": "29999880 193 29999860"},
-        "Sell": {"n": 64, "$": 1, "p": "29999880 192 29999860"},
+        "Buy": {"n": 32, "$": 1, "p": "22 114 32"},
+        "Sell": {"n": 64, "$": 1, "p": "22 113 32"},
     },
     "gravel": {
         "Name": ["Gravel","Gravel"],
-        "Buy": {"n": 16, "$": 1, "p": "29999880 193 29999859"},
-        "Sell": {"n": 32, "$": 1, "p": "29999880 192 29999859"},
+        "Buy": {"n": 16, "$": 1, "p": "22 114 31"},
+        "Sell": {"n": 32, "$": 1, "p": "22 113 31"},
     },
     "granite": {
         "Name": ["Granite","Granite"],
-        "Buy": {"n": 16, "$": 1, "p": "29999880 193 29999858"},
-        "Sell": {"n": 32, "$": 1, "p": "29999880 192 29999858"},
+        "Buy": {"n": 16, "$": 1, "p": "22 114 30"},
+        "Sell": {"n": 32, "$": 1, "p": "22 113 30"},
     },
     "andesite": {
         "Name": ["Andesite","Andesite"],
-        "Buy": {"n": 16, "$": 1, "p": "29999880 193 29999857"},
-        "Sell": {"n": 32, "$": 1, "p": "29999880 192 29999857"},
+        "Buy": {"n": 16, "$": 1, "p": "22 114 29"},
+        "Sell": {"n": 32, "$": 1, "p": "22 113 29"},
     },
     "diorite": {
         "Name": ["Dorito","Doritos"],
-        "Buy": {"n": 16, "$": 1, "p": "29999880 193 29999856"},
-        "Sell": {"n": 32, "$": 1, "p": "29999880 192 29999856"},
+        "Buy": {"n": 16, "$": 1, "p": "22 114 28"},
+        "Sell": {"n": 32, "$": 1, "p": "22 113 28"},
     },
     # Misc
     "bookshelf": {
         "Name": ["Bookshelf","Bookshelves"],
-        "Buy": {"n": 1, "$": 8, "p": "29999884 193 29999865"},
-        "Sell": {"n": 1, "$": 3, "p": "29999884 192 29999865"},
+        "Buy": {"n": 1, "$": 8, "p": "26 114 37"},
+        "Sell": {"n": 1, "$": 3, "p": "26 113 37"},
     },
     "tnt": {
         "Name": ["TNT","TNTs"],
-        "Buy": {"n": 1, "$": 8, "p": "29999884 193 29999864"},
-        "Sell": {"n": 1, "$": 3, "p": "29999884 192 29999864"},
+        "Buy": {"n": 1, "$": 8, "p": "26 114 36"},
+        "Sell": {"n": 1, "$": 3, "p": "26 113 36"},
     },
     # Food
     "beef": {
         "Name": ["Beef","Beef"],
-        "Sell": {"n": 4, "$": 1, "p": "29999894 192 29999861"},
+        "Sell": {"n": 4, "$": 1, "p": "36 113 33"},
     },
     "cooked_beef": {
         "Name": ["Steak","Steaks"],
-        "Buy": {"n": 8, "$": 5, "p": "29999894 193 29999861"},
+        "Buy": {"n": 8, "$": 5, "p": "36 114 33"},
     },
     "porkchop": {
         "Name": ["Porkchop","Porkchops"],
-        "Sell": {"n": 4, "$": 1, "p": "29999894 192 29999862"},
+        "Sell": {"n": 4, "$": 1, "p": "36 113 34"},
     },
     "cooked_porkchop": {
         "Name": ["Smoked Pork","Smoked Pork"],
-        "Buy": {"n": 8, "$": 5, "p": "29999894 193 29999862"},
+        "Buy": {"n": 8, "$": 5, "p": "36 114 34"},
     },
     "mutton": {
         "Name": ["Mutton","Mutton"],
-        "Sell": {"n": 5, "$": 1, "p": "29999894 192 29999863"},
+        "Sell": {"n": 5, "$": 1, "p": "36 113 35"},
     },
     "cooked_mutton": {
         "Name": ["Seared Mutton","Seared Mutton"],
-        "Buy": {"n": 2, "$": 1, "p": "29999894 193 29999863"},
+        "Buy": {"n": 2, "$": 1, "p": "36 114 35"},
     },
     "chicken": {
         "Name": ["Chicken","Chicken"],
-        "Sell": {"n": 5, "$": 1, "p": "29999894 192 29999864"},
+        "Sell": {"n": 5, "$": 1, "p": "36 113 36"},
     },
     "cooked_chicken": {
         "Name": ["Fried Chicken","Fried Chicken"],
-        "Buy": {"n": 2, "$": 1, "p": "29999894 193 29999864"},
+        "Buy": {"n": 2, "$": 1, "p": "36 114 36"},
     },
     "rabbit": {
         "Name": ["Rabbit","Rabbits"],
-        "Sell": {"n": 5, "$": 1, "p": "29999894 192 29999865"},
+        "Sell": {"n": 5, "$": 1, "p": "36 113 37"},
     },
     "cooked_rabbit": {
         "Name": ["Cooked Rabbits","Cooked Rabbits"],
-        "Buy": {"n": 2, "$": 1, "p": "29999894 193 29999865"},
+        "Buy": {"n": 2, "$": 1, "p": "36 114 37"},
     },
     "cod": {
         "Name": ["Cod","Cod"],
-        "Sell": {"n": 4, "$": 1, "p": "29999894 192 29999855"},
+        "Sell": {"n": 4, "$": 1, "p": "36 113 27"},
     },
     "cooked_cod": {
         "Name": ["Grilled Cod","Grilled Cod"],
-        "Buy": {"n": 1, "$": 2, "p": "29999894 193 29999855"},
+        "Buy": {"n": 1, "$": 2, "p": "36 114 27"},
     },
     "salmon": {
         "Name": ["Salmon","Salmon"],
-        "Sell": {"n": 4, "$": 1, "p": "29999894 192 29999854"},
+        "Sell": {"n": 4, "$": 1, "p": "36 113 26"},
     },
     "cooked_salmon": {
         "Name": ["Grilled Salmon","Grilled Salmon"],
-        "Buy": {"n": 1, "$": 2, "p": "29999894 193 29999854"},
+        "Buy": {"n": 1, "$": 2, "p": "36 114 26"},
     },
     "pufferfish": {
         "Name": ["Pufferfish","Pufferfish"],
-        "Buy": {"n": 1, "$": 4, "p": "29999894 193 29999853"},
-        "Sell": {"n": 2, "$": 1, "p": "29999894 192 29999853"},
+        "Buy": {"n": 1, "$": 4, "p": "36 114 25"},
+        "Sell": {"n": 2, "$": 1, "p": "36 113 25"},
     },
     "tropicalfish": {
         "Name": ["Tropical Fish","Tropical Fish"],
-        "Buy": {"n": 1, "$": 4, "p": "29999894 193 29999852"},
-        "Sell": {"n": 2, "$": 1, "p": "29999894 192 29999852"},
-    },
-    "golden_apple": {
-        "Name": ["Gold Apple","Gold Apples"],
-        "Buy": {"n": 1, "$": 25, "p": "29999888 193 29999859"},
-    },
-    "enchanted_golden_apple": {
-        "Name": ["Notch Apple","Notch Apples"],
-        "Buy": {"n": 1, "$": 300, "p": "29999888 192 29999859"},
+        "Buy": {"n": 1, "$": 4, "p": "36 114 24"},
+        "Sell": {"n": 2, "$": 1, "p": "36 113 24"},
     },
     "apple": {
         "Name": ["Apple","Apples"],
-        "Buy": {"n": 3, "$": 1, "p": "29999898 193 29999861"},
-        "Sell": {"n": 8, "$": 1, "p": "29999898 192 29999861"},
+        "Buy": {"n": 3, "$": 1, "p": "40 114 33"},
+        "Sell": {"n": 8, "$": 1, "p": "40 113 33"},
     },
     "carrot": {
         "Name": ["Carrot","Carrots"],
-        "Buy": {"n": 4, "$": 1, "p": "29999898 193 29999860"},
-        "Sell": {"n": 8, "$": 1, "p": "29999898 192 29999860"},
+        "Buy": {"n": 4, "$": 1, "p": "40 114 32"},
+        "Sell": {"n": 8, "$": 1, "p": "40 113 32"},
     },
     "potato": {
         "Name": ["Potato","Potatoes"],
-        "Buy": {"n": 4, "$": 1, "p": "29999898 193 29999859"},
-        "Sell": {"n": 8, "$": 1, "p": "29999898 192 29999859"},
+        "Buy": {"n": 4, "$": 1, "p": "40 114 31"},
+        "Sell": {"n": 8, "$": 1, "p": "40 113 31"},
     },
     "beet_root": {
         "Name": ["Beet","Beets"],
-        "Buy": {"n": 4, "$": 1, "p": "29999898 193 29999858"},
-        "Sell": {"n": 8, "$": 1, "p": "29999898 192 29999858"},
+        "Buy": {"n": 4, "$": 1, "p": "40 114 30"},
+        "Sell": {"n": 8, "$": 1, "p": "40 113 30"},
     },
     "wheat": {
         "Name": ["Wheat","Wheat"],
-        "Buy": {"n": 8, "$": 1, "p": "29999898 193 29999857"},
-        "Sell": {"n": 16, "$": 1, "p": "29999898 192 29999857"},
+        "Buy": {"n": 8, "$": 1, "p": "40 114 29"},
+        "Sell": {"n": 16, "$": 1, "p": "40 113 29"},
     },
     "sugar_cane": {
         "Name": ["Sugarcane","Sugarcane"],
-        "Buy": {"n": 8, "$": 1, "p": "29999898 193 29999856"},
-        "Sell": {"n": 16, "$": 1, "p": "29999898 192 29999856"},
+        "Buy": {"n": 8, "$": 1, "p": "40 114 28"},
+        "Sell": {"n": 16, "$": 1, "p": "40 113 28"},
     },
     "melon": {
         "Name": ["Watermelon","Watermelons"],
-        "Buy": {"n": 8, "$": 5, "p": "29999896 193 29999856"},
+        "Buy": {"n": 8, "$": 5, "p": "38 114 28"},
     },
     "melon_slice": {
         "Name": ["Melon Slice","Melon Slices"],
-        "Sell": {"n": 20, "$": 1, "p": "29999896 192 29999856"},
+        "Sell": {"n": 20, "$": 1, "p": "38 113 28"},
     },
     "pumpkin": {
         "Name": ["Pumpkin","Pumpkins"],
-        "Buy": {"n": 4, "$": 1, "p": "29999896 193 29999857"},
-        "Sell": {"n": 16, "$": 1, "p": "29999896 192 29999857"},
+        "Buy": {"n": 4, "$": 1, "p": "38 114 29"},
+        "Sell": {"n": 16, "$": 1, "p": "38 113 29"},
     },
     "cactus": {
         "Name": ["Cactus","Cacti"],
-        "Buy": {"n": 16, "$": 1, "p": "29999896 193 29999858"},
-        "Sell": {"n": 32, "$": 1, "p": "29999896 192 29999858"},
+        "Buy": {"n": 16, "$": 1, "p": "38 114 30"},
+        "Sell": {"n": 32, "$": 1, "p": "38 113 30"},
     },
     "chorus_flower": {
         "Name": ["Chorus Flower","Chorus Flowers"],
-        "Buy": {"n": 1, "$": 25, "p": "29999896 193 29999859"},
-        "Sell": {"n": 4, "$": 1, "p": "29999896 192 29999859"},
+        "Buy": {"n": 1, "$": 25, "p": "38 114 31"},
+        "Sell": {"n": 4, "$": 1, "p": "38 113 31"},
     },
     "red_mushroom": {
         "Name": ["Red Mushroom","Red Mushrooms"],
-        "Buy": {"n": 5, "$": 3, "p": "29999896 193 29999860"},
-        "Sell": {"n": 3, "$": 1, "p": "29999896 192 29999860"},
+        "Buy": {"n": 5, "$": 3, "p": "38 114 32"},
+        "Sell": {"n": 3, "$": 1, "p": "38 113 32"},
     },
     "brown_mushroom": {
         "Name": ["Brown Mushroom","Brown Mushrooms"],
-        "Buy": {"n": 5, "$": 3, "p": "29999896 193 29999861"},
-        "Sell": {"n": 3, "$": 1, "p": "29999896 192 29999861"},
+        "Buy": {"n": 5, "$": 3, "p": "38 114 33"},
+        "Sell": {"n": 3, "$": 1, "p": "38 113 33"},
     },
     "coal": {
         "Name": ["Coal","Coal"],
-        "Buy": {"n": 4, "$": 1, "p": "29999882 193 29999852"},
-        "Sell": {"n": 8, "$": 1, "p": "29999882 192 29999852"},
+        "Buy": {"n": 4, "$": 1, "p": "24 114 24"},
+        "Sell": {"n": 8, "$": 1, "p": "24 113 24"},
     },
     "flint": {
         "Name": ["Flint","Flint"],
-        "Buy": {"n": 8, "$": 1, "p": "29999882 193 29999853"},
-        "Sell": {"n": 4, "$": 1, "p": "29999882 192 29999853"},
+        "Buy": {"n": 8, "$": 1, "p": "24 114 25"},
+        "Sell": {"n": 4, "$": 1, "p": "24 113 25"},
     },
     "clay_ball": {
         "Name": ["Clay","Clay"],
-        "Buy": {"n": 4, "$": 1, "p": "29999882 193 29999854"},
-        "Sell": {"n": 8, "$": 1, "p": "29999882 192 29999854"},
+        "Buy": {"n": 4, "$": 1, "p": "24 114 26"},
+        "Sell": {"n": 8, "$": 1, "p": "24 113 26"},
     },
     "glowstone_dust": {
         "Name": ["Glowstone Dust","Glowstone Dust"],
-        "Buy": {"n": 4, "$": 1, "p": "29999882 193 29999855"},
-        "Sell": {"n": 8, "$": 1, "p": "29999882 192 29999855"},
+        "Buy": {"n": 4, "$": 1, "p": "24 114 27"},
+        "Sell": {"n": 8, "$": 1, "p": "24 113 27"},
     },
     # STORAGE
     "chest": {
         "Name": ["Chest","Chests"],
-        "Buy": {"n": 1, "$": 500, "p": "29999884 193 29999852"},
-        "Sell": {"n": 1, "$": 100, "p": "29999884 192 29999852"},
+        "Buy": {"n": 1, "$": 500, "p": "26 114 24"},
+        "Sell": {"n": 1, "$": 100, "p": "26 113 24"},
     },
     "barrel": {
         "Name": ["Barrel","Barrels"],
-        "Buy": {"n": 1, "$": 500, "p": "29999884 193 29999853"},
-        "Sell": {"n": 1, "$": 100, "p": "29999884 192 29999853"},
+        "Buy": {"n": 1, "$": 500, "p": "26 114 25"},
+        "Sell": {"n": 1, "$": 100, "p": "26 113 25"},
     },
     "dispenser": {
         "Name": ["Dispenser","Dispensers"],
-        "Buy": {"n": 1, "$": 200, "p": "29999884 193 29999854"},
-        "Sell": {"n": 1, "$": 100, "p": "29999884 192 29999854"},
+        "Buy": {"n": 1, "$": 200, "p": "26 114 27"},
+        "Sell": {"n": 1, "$": 100, "p": "26 113 27"},
     },
     "dropper": {
         "Name": ["Dropper","Droppers"],
-        "Buy": {"n": 1, "$": 200, "p": "29999884 193 29999855"},
-        "Sell": {"n": 1, "$": 100, "p": "29999884 192 29999855"},
+        "Buy": {"n": 1, "$": 200, "p": "26 114 28"},
+        "Sell": {"n": 1, "$": 100, "p": "26 113 28"},
     },
 
     "player_head": {
         "Name": ["Money Bag","Money Bag"],
-        "Sell": {"n": 1, "$": 100, "p": "29999889 192 29999852"},
+        "Sell": {"n": 1, "$": 100, "p": "31 113 24"},
     },
     "clock": {
         "Name": ["Smacker","Smackers"],
-        "Sell": {"n": 1, "$": 1, "p": "29999887 192 29999852"},
+        "Sell": {"n": 1, "$": 1, "p": "29 113 24"},
     },
     "Money Bag": {
         "TextLines": [
@@ -304,9 +296,9 @@ shopDeals = {
         ],
         "Custom": {
             "$": 100,
-            "p": "29999889 193 29999852",
+            "p": "31 114 24",
             "cmds": [
-                """give @s minecraft:player_head{SkullOwner:{Id:"da608428-e220-435c-9669-2fe3c0575458",Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTM2ZTk0ZjZjMzRhMzU0NjVmY2U0YTkwZjJlMjU5NzYzODllYjk3MDlhMTIyNzM1NzRmZjcwZmQ0ZGFhNjg1MiJ9fX0="}]}}} 1""",
+                """give @s minecraft:player_head{display:{Name:'[{"text":"Money Bag","italic":false,"bold":true,"color":"yellow"}]',Lore:['[{"text":"Money itself is not evil.","italic":false,"color":"dark_purple"}]','[{"text":"It\'s a great tool, one to be used wisely.","italic":false,"color":"dark_purple"},{"text":"","italic":false,"color":"dark_purple"}]','[{"text":"","italic":false,"color":"dark_purple"}]','[{"text":"$100","italic":false,"color":"white"}]']},SkullOwner:{Id:[I;-631208920,-501202084,-1771491357,-1068018600],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTM2ZTk0ZjZjMzRhMzU0NjVmY2U0YTkwZjJlMjU5NzYzODllYjk3MDlhMTIyNzM1NzRmZjcwZmQ0ZGFhNjg1MiJ9fX0="}]}}} 1""",
             ]
         }
     },
@@ -317,7 +309,7 @@ shopDeals = {
         ],
         "Custom": {
             "$": 1,
-            "p": "29999887 193 29999852",
+            "p": "29 114 24",
             "cmds": [
                 """give @s clock{display:{Name:"[{\"text\":\"Smacker\",\"italic\":false,\"bold\":true,\"color\":\"yellow\"}]",Lore:["[{\"text\":\"We gotta keep doing this Bob.\",\"italic\":false}]"]}} 1""",
             ]
@@ -331,7 +323,7 @@ shopDeals = {
             {"text": "$1 each"},
         ],
         "Custom": {
-            "p": "29999888 192 29999852",
+            "p": "30 113 24",
             "cmds": [
                 "execute as @s store result score @s TEMPVALUE run clear @s minecraft:clock 0",
                 "execute if score @s TEMPVALUE matches 1.. run clear @s minecraft:clock",
