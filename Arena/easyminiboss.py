88f102d4b3be66679893bb05f682184efec854e8
AlexanderGroeger <46076580+AlexanderGroeger@users.noreply.github.com>
2021-01-30 19:30:49 -0500

    added miniboss

---
+++ Arena/easyminiboss.py
@@ -0,0 +1,79 @@
+rounds = [
+    # Round 10
+    {
+        "bonus": 3,
+        "loot": "loot:arena/tier1mob",
+        "time": 300,
+        "waves": [
+            {
+                "delay": 0,
+                "mobs": [
+                    {
+                        "type": "skeleton",
+                        "weapons": "bow",
+                        "passengers": {
+                            "type": "skeleton",
+                            "weapons": "bow",
+                            "armor": {
+                                "head": "leather_helmet",
+                                "body": "leather_chestplate",
+                                "legs": "leather_leggings",
+                                "feet": "leather_boots",
+                            },
+                            "passengers": {
+                                "type": "skeleton",
+                                "weapons": "bow",
+                                "armor": {
+                                    "head": "golden_helmet",
+                                    "body": "golden_chestplate",
+                                    "legs": "golden_leggings",
+                                    "feet": "golden_boots",
+                                },
+                                "passengers": {
+                                    "type": "skeleton",
+                                    "weapons": "bow",
+                                    "armor": {
+                                        "head": "chainmail_helmet",
+                                        "body": "chainmail_chestplate",
+                                        "legs": "chainmail_leggings",
+                                        "feet": "chainmail_boots",
+                                    },
+                                    "passengers": {
+                                        "type": "skeleton",
+                                        "weapons": "bow",
+                                        "armor": {
+                                            "head": "iron_helmet",
+                                            "body": "iron_chestplate",
+                                            "legs": "iron_leggings",
+                                            "feet": "iron_boots",
+                                        },
+                                        "passengers": {
+                                            "type": "skeleton",
+                                            "weapons": "bow",
+                                            "armor": {
+                                                "head": "diamond_helmet",
+                                                "body": "diamond_chestplate",
+                                                "legs": "diamond_leggings",
+                                                "feet": "diamond_boots",
+                                            },
+                                            "passengers": {
+                                                "type": "skeleton",
+                                                "weapons": "bow",
+                                                "armor": {
+                                                    "head": "netherite_helmet",
+                                                    "body": "netherite_chestplate",
+                                                    "legs": "netherite_leggings",
+                                                    "feet": "netherite_boots",
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+]
