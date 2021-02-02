79255da1a3fee1fb1259197bac744f2cd91399a0
AlexanderGroeger <46076580+AlexanderGroeger@users.noreply.github.com>
2021-02-02 13:21:17 -0500

    ye

---
+++ Arena/easyboss.py
@@ -0,0 +1,23 @@
+rounds = [
+    # Round 20
+    {
+        "bonus": 3,
+        "time": 300,
+        "waves": [
+            {
+                "delay": 0,
+                "mobs": [
+                    {
+                        "type": "wither_skeleton",
+                        "tags": "boss",
+                        "armor": {
+                            "head": "spawner",
+                            "body": "netherite_chestplate",
+                        },
+                        "effects": ("invisibility",1)
+                    },
+                ],
+            },
+        ]
+    },
+]
